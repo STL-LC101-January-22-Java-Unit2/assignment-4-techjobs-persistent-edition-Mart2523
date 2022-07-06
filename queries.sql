6054ac1a4219e58bcf2ffaa7a7fafc69e4a4c7d0
Cade M <you@example.com>
2022-07-05 19:34:48 -0500

    Finished SQL Queries

--- queries.sql
+++ queries.sql
@@ -2,7 +2,13 @@
 --Columns: Id int, Employer varchar, Name varchar, Skills varchar
 
 -- Part 2: Test it with SQL
-
+SELECT name FROM employer WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
-
+DROP TABLE JOB
 -- Part 4: Test it with SQL
+SELECT *
+FROM skill
+LEFT JOIN job_skills
+ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;

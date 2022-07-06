8d2a4e1818e29c2c7846185eda8903dcd86c9ba6
Cade M <you@example.com>
2022-07-05 19:41:32 -0500

    Updated 4th Query test

--- queries.sql
+++ queries.sql
@@ -8,7 +8,6 @@ DROP TABLE job
 -- Part 4: Test it with SQL
 SELECT *
 FROM skill
-LEFT JOIN job_skills
-ON skill.id = job_skills.skills_id
+INNER JOIN job_skills ON job_skills.skills_id = skill.id
 WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
